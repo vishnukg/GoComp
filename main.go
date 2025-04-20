bc789e93763ebe23ba4b31e09fb4e8440bba2aba
Vishnu Ganesan <vishnukg@gmail.com>
2025-04-20 15:28:54 +1000

    Lazy

--- main.go
+++ main.go
@@ -4,4 +4,15 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World")
+	pers := person{"Vishnu", 90}
+	fmt.Println(pers.getNmae())
+}
+
+type person struct {
+	name string
+	age  int
+}
+
+func (p person) getNmae() string {
+	return p.name
 }
