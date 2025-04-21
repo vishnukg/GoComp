edd527ffbb10fac044a49f31655113c9ab02e514
Vishnu Ganesan <vishnukg@gmail.com>
2025-04-21 22:02:39 +1000

    LoopS

--- main.go
+++ main.go
@@ -1,18 +1,12 @@
+// package main entry point
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, World")
-	pers := person{"Vishnu", 90}
-	fmt.Println(pers.getNmae())
-}
-
-type person struct {
-	name string
-	age  int
-}
-
-func (p person) getNmae() string {
-	return p.name
+	for i := range 5 {
+		fmt.Printf("loop count:%d\n", i)
+	}
 }
